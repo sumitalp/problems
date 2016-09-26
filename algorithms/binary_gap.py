52214c8027d043830d462c0888a032ca44d263b7
Ahsanuzzaman Khan <ahsan.khan434@gmail.com>
2016-09-26 13:17:35 +0600

    Codility 'Binary Gap' problem solution.

---
+++ algorithms/binary_gap.py
@@ -0,0 +1,28 @@
+# Codility 'Binary Gap' problem
+
+def solution(N):
+    # write your code in Python 2.7
+    max_gap = 0
+    curr_gap = 0
+    
+    while N > 0 and N%2 ==0:
+        N //= 2
+        
+    while N > 0:
+        r = N%2
+        
+        if r==0:
+            # Inside gap
+            curr_gap += 1
+        else:
+            # Gap ends
+            if curr_gap != 0:
+                max_gap = max(curr_gap, max_gap)
+                curr_gap = 0
+                
+        N //= 2
+    
+    return max_gap
+
+# Worst Complexity O(log(n))
+# Worst space complexity O(1)
