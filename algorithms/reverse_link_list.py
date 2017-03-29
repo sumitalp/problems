2cfe809a62df3878aa64191436275013ce1769f5
Ahsanuzzaman Khan <ahsan.khan434@gmail.com>
2017-03-29 16:44:41 +0600

    Hacker Rank 'Reverse Link list' problem solved.

--- algorithms/reverse_link_list.py
+++ algorithms/reverse_link_list.py
@@ -1,9 +1,9 @@
-class Node(object):
-    def __init__(self, data=None, next_node=None):
-        self.data = data
-        self.next = next_node
-
-def ReversePrint(head):
-    if head is not None:
-        ReversePrint(head.next)
-        print(head.data)
+def Reverse(head):
+    prev = None
+    current = head
+    while(current is not None):
+        next = current.next
+        current.next = prev
+        prev = current
+        current = next
+    return prev
