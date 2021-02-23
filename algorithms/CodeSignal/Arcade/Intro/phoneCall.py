606c7fc43ed918d772feb5210a2df2f600ed9249
Ahsanuzzaman Khan <ahsan.khan434@gmail.com>
2021-02-23 12:00:00 +0600

    CodeSignal: Arcade intro problem phoneCall has solved.

--- algorithms/CodeSignal/Arcade/Intro/phoneCall.py
+++ algorithms/CodeSignal/Arcade/Intro/phoneCall.py
@@ -68,3 +68,12 @@ def phoneCall(min1, min2_10, min11, s):
         
     
 assert phoneCall(3, 1, 2, 20) == 14
+
+'''
+def phoneCall(min1, min2_10, min11, s):
+    if s<min1:
+        return 0
+    elif s>=min1 and s<=min1+9*min2_10:
+        return 1+(s-min1)//min2_10
+    return 10+(s-min1-9*min2_10)//min11
+'''
